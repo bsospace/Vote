5ea50f4833e6ab6e64a35685898baa311a6b24da
boytur <mask30254@gmail.com>
2025-02-17 00:44:42 +0700

    ✨ feat: enhance configuration management with environment variable validation and add user-related interfaces

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,9 +1,48 @@
-import dotenv from "dotenv";
-
+import dotenv from 'dotenv';
 dotenv.config();
 
-export default {
-  port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL,
-  jwtSecret: process.env.JWT_SECRET || "your-secret-key",
+/**
+ * Ensures that the specified environment variable is present.
+ * @param key - The name of the environment variable.
+ * @returns The value of the environment variable.
+ * @throws Error if the environment variable is not set.
+ */
+function requireEnv(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable "${key}" is required but was not found.`);
+  }
+  return value;
+}
+
+// Example usage with your configuration
+export const envConfig = {
+  appPort: parseInt(requireEnv('APP_PORT')),
+  nodeEnv: requireEnv('NODE_ENV'),
+  seScret: requireEnv('SECRET'),
+  app:{
+    frontendUrl: requireEnv('FRONTEND_URL'),
+    backendUrl: requireEnv('BACKEND_URL'),
+    serviceName: requireEnv('SERVICE_NAME'),
+    cookieDomain: requireEnv('COOKIE_DOMAIN'),
+  },
+  database: {
+    url: requireEnv('DATABASE_URL'),
+    user: requireEnv('POSTGRES_USER'),
+    password: requireEnv('POSTGRES_PASSWORD'),
+    dbName: requireEnv('POSTGRES_DB'),
+    port: parseInt(requireEnv('POSTGRES_PORT')),
+  },
+  redis: {
+    port: parseInt(requireEnv('REDIS_PORT')),
+    password: requireEnv('REDIS_PASSWORD'),
+    host: requireEnv('REDIS_HOST')
+  },
+  pgAdmin: {
+    email: requireEnv('PGADMIN_DEFAULT_EMAIL'),
+    password: requireEnv('PGADMIN_DEFAULT_PASSWORD'),
+    port: parseInt(requireEnv('PGADMIN_PORT')),
+  },
+  openIdApi: requireEnv('OPENID_API'),
+  prodBackEndUrl: requireEnv('PROD_BACKEND_URL'),
 };
---
+++ backend/src/interface.ts
@@ -0,0 +1,143 @@
+export interface IUser {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    avatar?: string;
+    events?: IEvent[];
+    polls?: IPoll[];
+    whitelist?: IWhitelistUser[];
+    votes?: IVote[];
+    userVotes?: IVoteRestriction[];
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IEvent {
+    id: string;
+    name: string;
+    description?: string;
+    userId: string;
+    owner?: IUser;
+    polls?: IPoll[];
+    whitelist?: IWhitelistUser[];
+    guests?: IGuest[];
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IPoll {
+    id: string;
+    eventId?: string | null;
+    event?: IEvent;
+    userId: string;
+    owner?: IUser;
+    question: string;
+    description?: string;
+    isPublic: boolean;
+    startVoteAt: Date;
+    endVoteAt: Date;
+    isVoteEnd: boolean;
+    banner?: string;
+    publishedAt?: Date | null;
+    options?: IOption[];
+    whitelist?: IWhitelistUser[];
+    votes?: IVote[];
+    guestVotes?: IGuest[];
+    voteRestrict?: IVoteRestriction[];
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IOption {
+    id: string;
+    text: string;
+    banner?: string;
+    description?: string;
+    pollId: string;
+    poll?: IPoll;
+    votes?: IVote[];
+    voteRestrict?: IVoteRestriction[];
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IWhitelistUser {
+    id: string;
+    userId: string;
+    user?: IUser;
+    eventId?: string | null;
+    event?: IEvent;
+    pollId?: string | null;
+    poll?: IPoll;
+    point: number;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IGuest {
+    id: string;
+    name: string;
+    key: string;
+    eventId?: string | null;
+    event?: IEvent;
+    pollId?: string | null;
+    poll?: IPoll;
+    votes?: IVote[];
+    guestVotes?: IVoteRestriction[];
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IVoteRestriction {
+    id: string;
+    userId?: string | null;
+    user?: IUser;
+    guestId?: string | null;
+    guest?: IGuest;
+    pollId: string;
+    poll?: IPoll;
+    optionId: string;
+    option?: IOption;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface IVote {
+    id: string;
+    pollId: string;
+    poll?: IPoll;
+    optionId: string;
+    option?: IOption;
+    userId?: string | null;
+    user?: IUser;
+    guestId?: string | null;
+    guest?: IGuest;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    dataLogs?: DataLog[];
+  }
+  
+  export interface DataLog {
+    action: string;
+    createdAt: Date;
+    updatedAt: Date;
+    createdBy: string;
+    meta: string[];
+  }
+  
