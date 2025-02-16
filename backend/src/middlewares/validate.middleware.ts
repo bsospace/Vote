dd5569aa8c622db25dc5db213cbac05651ad60f8
boytur <mask30254@gmail.com>
2025-02-17 00:45:11 +0700

    ✨ feat: add request validation middleware and validators for events and polls

---
+++ backend/src/middlewares/validate.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+
+/***
+ * This function validates the request and returns an error if the request is invalid.
+ * @param req - The Express request object.
+ * @param res - The Express response object.
+ * @param next - The next middleware function.
+ */
+
+export const validateRequest = (req: Request, res: Response, next: NextFunction): any => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid request",
+      error: errors.array()[0].msg,
+    });
+  }
+  next();
+};
