a359f64ac46e72f1ff0c268cf03dbb6e93a6530a
boytur <mask30254@gmail.com>
2025-02-19 14:25:03 +0700

    🔧 chore[Database]: add tables for User, Event, Poll, Option, WhitelistUser, Guest, VoteRestriction, Vote, and FailedJob with relationships

--- backend/prisma/migrations/20250219063421_/migration.sql
+++ backend/prisma/migrations/20250219063421_/migration.sql
@@ -41,6 +41,7 @@ CREATE TABLE "Poll" (
     "isVoteEnd" BOOLEAN NOT NULL DEFAULT false,
     "banner" TEXT,
     "publishedAt" TIMESTAMP(3),
+    "voteCount" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "deletedAt" TIMESTAMP(3),
@@ -123,11 +124,26 @@ CREATE TABLE "Vote" (
     CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "FailedJob" (
+    "id" TEXT NOT NULL,
+    "jobId" TEXT NOT NULL,
+    "queueName" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+    "error" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FailedJob_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "WhitelistUser_userId_eventId_key" ON "WhitelistUser"("userId", "eventId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FailedJob_jobId_key" ON "FailedJob"("jobId");
 
 -- AddForeignKey
 ALTER TABLE "Event" ADD CONSTRAINT "Event_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
